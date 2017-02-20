f2eb59cea908b43194f2a32c1e3804f6ab1e5f68
yangguangyu <xxgcxy123@163.com>
2017-02-21 00:14:47 +0800

    使用position来替换

--- GYPhotoBrowser/GYCollectionViewCell.m
+++ GYPhotoBrowser/GYCollectionViewCell.m
@@ -53,8 +53,8 @@
     layer.strokeEnd = 0;
     layer.strokeColor = [UIColor redColor].CGColor;
     layer.fillColor = [UIColor clearColor].CGColor;
-    layer.frame = CGRectMake((self.frame.size.width-80)*0.5, (self.frame.size.height-80)*0.5, 80, 80);
-//        layer.position = cell.imageView.center;
+    layer.frame = CGRectMake(0, 0, 80, 80);
+    layer.position = self.center;
     layer.backgroundColor = [UIColor blueColor].CGColor;
     [self.imageView.layer addSublayer:layer];
     
